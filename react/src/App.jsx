1b0582ced0c09bbf57e6aaca1c0fb07b18a1d23e
Edgar Melano <eamelano@gmail.com>
2023-05-22 15:53:20 -0700

    Navbar updated

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,15 +1,13 @@
-import { Container } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
 import SiteNav from "./components/navbar/SiteNav";
 
 function App() {
   return (
-    <>
+    <div className="Global d-flex min-vh-100">
       <Routes>
         <Route path="/*" element={<SiteNav />} />
       </Routes>
-      <Container fluid className="d-flex min-vh-100 bg-dark"></Container>
-    </>
+    </div>
   );
 }
 
--- react/src/components/navbar/SiteNav.jsx
+++ react/src/components/navbar/SiteNav.jsx
@@ -1,23 +1,57 @@
-import { Container, Navbar } from "react-bootstrap";
-
+import {
+  Container,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Image,
+  Button,
+} from "react-bootstrap";
 import React from "react";
+import "./sitenav.css";
+import pfp from "../../assets/images/ProfileAvatar.png";
 
 const SiteNavbar = () => {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="/">
-          <img
-            alt=""
-            src="/logo.svg"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{" "}
-          Project Slayer
-        </Navbar.Brand>
+    <>
+      <Container fluid className="pt-3">
+        <Navbar bg="secondary" variant="dark" expand="lg" sticky="top">
+          <div className="profile">
+            <Image alt="" src={pfp} fluid thumbnail />{" "}
+          </div>
+          <Container fluid>
+            <Navbar.Brand href="/">Slayer Legend</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse>
+              <Nav className="me-auto">
+                <Nav.Link>Beginner's Guide</Nav.Link>
+                <Nav.Link>Master Optimizer (WIP)</Nav.Link>
+                <Nav.Link>Builds</Nav.Link>
+                <NavDropdown title="Calculators" menuVariant="dark">
+                  <NavDropdown.Item>Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title="Resources" menuVariant="dark"></NavDropdown>
+              </Nav>
+              <Nav>
+                <Button variant="outline-success" className="me-2">
+                  Login
+                </Button>
+                <Button variant="outline-warning">Sign Up</Button>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </Container>
-    </Navbar>
+    </>
   );
 };
 
